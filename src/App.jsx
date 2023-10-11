421329580339fe53a033979e52751417ac0058e0
Sunny Pal <sunny2212102@akgec.ac.in>
2023-10-12 03:05:53 +0530

    layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [expenses, setExpenses] = useState([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+
+  const addExpense = () => {
+    if (description && amount && date) {
+      const newExpense = {
+        id: expenses.length + 1,
+        description,
+        amount: parseFloat(amount),
+        date,
+      };
+
+      setExpenses([...expenses, newExpense]);
+      setDescription('');
+      setAmount('');
+      setDate('');
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-100">
+      <div className="bg-blue-500 p-4">
+        <h1 className="text-white text-2xl font-bold text-center">Expense Tracker</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="p-4">
+        <h2 className="text-xl font-bold mb-4 text-blue-500">Add Expense</h2>
+        <div className="mb-4">
+          <label htmlFor="description" className="block font-semibold text-blue-500">Description:</label>
+          <input
+            type="text"
+            id="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="amount" className="block font-semibold text-blue-500">Amount:</label>
+          <input
+            type="number"
+            id="amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="date" className="block font-semibold text-blue-500">Date:</label>
+          <input
+            type="date"
+            id="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="border rounded w-full p-2"
+          />
+        </div>
+        <button onClick={addExpense} className="bg-blue-500 text-white p-2 rounded">Add Expense</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="p-4">
+        <h2 className="text-xl font-semibold mb-4 text-blue-500">Expense List</h2>
+        <ul>
+          {expenses.map((expense) => (
+            <li key={expense.id} className="border p-2 rounded mb-2">
+              <strong>{expense.description}</strong> - ${expense.amount} on {expense.date}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
