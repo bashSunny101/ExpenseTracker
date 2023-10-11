8ac5fcd2cf6fd9b7868cfc6f4eb2865dd98fbb94
Sunny Pal <sunny2212102@akgec.ac.in>
2023-10-12 04:32:06 +0530

    increased the width of the main element

--- src/App.jsx
+++ src/App.jsx
@@ -28,21 +28,14 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <div className="bg-blue-500 p-4">
-        <h1 className="text-2xl font-bold text-center">Expense Tracker</h1>
+        <h1 className="text-2xl font-bold text-center text-white">Expense Tracker</h1>
       </div>
 
       <div className="flex-grow flex justify-center items-center">
-        <div className="p-4">
-          <h2 className="text-xl font-semibold mb-4 text-blue-500">
-            Add Expense
-          </h2>
+        <div className="p-4 w-96">
+          <h2 className="text-xl font-semibold mb-4 text-blue-500">Add Expense</h2>
           <div className="mb-4">
-            <label
-              htmlFor="description"
-              className="block font-semibold text-black"
-            >
-              Description:
-            </label>
+            <label htmlFor="description" className="block font-semibold text-black">Description:</label>
             <input
               type="text"
               id="description"
@@ -52,9 +45,7 @@ function App() {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="amount" className="block font-semibold text-black">
-              Amount:
-            </label>
+            <label htmlFor="amount" className="block font-semibold text-black">Amount:</label>
             <input
               type="number"
               id="amount"
@@ -64,9 +55,7 @@ function App() {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="date" className="block font-semibold text-black">
-              Date:
-            </label>
+            <label htmlFor="date" className="block font-semibold text-black">Date:</label>
             <input
               type="date"
               id="date"
@@ -75,21 +64,18 @@ function App() {
               className="border rounded w-full p-2"
             />
           </div>
-          <button onClick={addExpense} className="bg-blue-500 p-2 rounded">
+          <button onClick={addExpense} className="bg-blue-500 p-2 rounded text-white w-full hover:bg-blue-600">
             Add Expense
           </button>
         </div>
       </div>
 
       <div className="p-4">
-        <h2 className="text-xl font-semibold mb-4 text-blue-500">
-          Expense List
-        </h2>
+        <h2 className="text-xl font-semibold mb-4 text-blue-500">Expense List</h2>
         <ul>
           {expenses.map((expense) => (
             <li key={expense.id} className="border p-2 rounded mb-2 text-black">
-              <strong>{expense.description}</strong> - ${expense.amount} on{" "}
-              {expense.date}
+              <strong>{expense.description}</strong> - ${expense.amount} on {expense.date}
             </li>
           ))}
         </ul>
@@ -97,7 +83,7 @@ function App() {
 
       <div className="p-4">
         <h2 className="text-xl font-semibold text-blue-500">Total Expenses:</h2>
-        <p className="text-black">${calculateTotal()}</p>
+        <p className="text-black text-xl">${calculateTotal()}</p>
       </div>
     </div>
   );
