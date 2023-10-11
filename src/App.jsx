b5cc03a99c16574336800175f4cc5862504e681e
Sunny Pal <sunny2212102@akgec.ac.in>
2023-10-12 03:54:52 +0530

    logic implemted

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
   const [expenses, setExpenses] = useState([]);
-  const [description, setDescription] = useState('');
-  const [amount, setAmount] = useState('');
-  const [date, setDate] = useState('');
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [date, setDate] = useState("");
 
   const addExpense = () => {
     if (description && amount && date) {
@@ -14,64 +14,90 @@ function App() {
         amount: parseFloat(amount),
         date,
       };
-
       setExpenses([...expenses, newExpense]);
-      setDescription('');
-      setAmount('');
-      setDate('');
+      setDescription("");
+      setAmount("");
+      setDate("");
     }
   };
 
+  const calculateTotal = () => {
+    return expenses.reduce((total, expense) => total + expense.amount, 0);
+  };
+
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 flex flex-col">
       <div className="bg-blue-500 p-4">
-        <h1 className="text-white text-2xl font-bold text-center">Expense Tracker</h1>
+        <h1 className="text-2xl font-bold text-center">Expense Tracker</h1>
+      </div>
+
+      <div className="flex-grow flex justify-center items-center">
+        <div className="p-4">
+          <h2 className="text-xl font-semibold mb-4 text-blue-500">
+            Add Expense
+          </h2>
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block font-semibold text-black"
+            >
+              Description:
+            </label>
+            <input
+              type="text"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="border rounded w-full p-2"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="amount" className="block font-semibold text-black">
+              Amount:
+            </label>
+            <input
+              type="number"
+              id="amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              className="border rounded w-full p-2"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="date" className="block font-semibold text-black">
+              Date:
+            </label>
+            <input
+              type="date"
+              id="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              className="border rounded w-full p-2"
+            />
+          </div>
+          <button onClick={addExpense} className="bg-blue-500 p-2 rounded">
+            Add Expense
+          </button>
+        </div>
       </div>
 
       <div className="p-4">
-        <h2 className="text-xl font-bold mb-4 text-blue-500">Add Expense</h2>
-        <div className="mb-4">
-          <label htmlFor="description" className="block font-semibold text-blue-500">Description:</label>
-          <input
-            type="text"
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="border rounded w-full p-2"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="amount" className="block font-semibold text-blue-500">Amount:</label>
-          <input
-            type="number"
-            id="amount"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            className="border rounded w-full p-2"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="date" className="block font-semibold text-blue-500">Date:</label>
-          <input
-            type="date"
-            id="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="border rounded w-full p-2"
-          />
-        </div>
-        <button onClick={addExpense} className="bg-blue-500 text-white p-2 rounded">Add Expense</button>
-      </div>
-
-      <div className="p-4">
-        <h2 className="text-xl font-semibold mb-4 text-blue-500">Expense List</h2>
+        <h2 className="text-xl font-semibold mb-4 text-blue-500">
+          Expense List
+        </h2>
         <ul>
           {expenses.map((expense) => (
-            <li key={expense.id} className="border p-2 rounded mb-2">
-              <strong>{expense.description}</strong> - ${expense.amount} on {expense.date}
+            <li key={expense.id} className="border p-2 rounded mb-2 text-black">
+              <strong>{expense.description}</strong> - ${expense.amount} on{" "}
+              {expense.date}
             </li>
           ))}
         </ul>
+      </div>
+
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-blue-500">Total Expenses:</h2>
+        <p className="text-black">${calculateTotal()}</p>
       </div>
     </div>
   );
